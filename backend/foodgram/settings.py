004558604f082e1e8dae0e8b8fc64a1400427b53
Pavel Osipov <osiov25@gmail.com>
2025-11-22 20:31:13 +0600

    Initial commit: Foodgram project complete

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-dev-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1 [::1]').split(' ')
-
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(' ')
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
